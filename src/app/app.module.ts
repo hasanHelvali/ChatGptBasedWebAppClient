a59fa54b3f1c8ae749c22201cb00116846cad13e
hasanHelvali <114858957+hasanHelvali@users.noreply.github.com>
2023-10-31 00:47:06 +0300

    son

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     BrowserAnimationsModule,
     NgxSpinnerModule
   ],
-  providers: [ChatGptCompletionService,ChatGptImageService,{provide:"baseurl",useValue:"https://192.168.134.81:443/api/"}],
+  providers: [ChatGptCompletionService,ChatGptImageService,{provide:"baseurl",useValue:"https://gptbasedapi/api/"}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
